b384bab5139f2a6bb2e3cea60f0ee891b03b02a9
simioyin222 <simi.oyinkolade@gmail.com>
2024-02-03 00:27:02 -0600

    fixing project organization and directory

---
+++ HairSalon/Data/SalonDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Data
+{
+    public class SalonDbContext : DbContext
+    {
+        public SalonDbContext(DbContextOptions<SalonDbContext> options) : base(options) { }
+
+        public DbSet<Stylist> Stylists { get; set; }
+        public DbSet<Client> Clients { get; set; }
+    }
+}
