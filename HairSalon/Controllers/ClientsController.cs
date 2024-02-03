81a3936d35a0361da02372b6b4be5d9065425c07
simioyin222 <simi.oyinkolade@gmail.com>
2024-02-03 02:00:20 -0600

    add edit function in client controller

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -42,4 +42,19 @@ namespace HairSalon.Controllers
             Client thisClient = _db.Clients.Include(client => client.Stylist).FirstOrDefault(client => client.ClientId == id);
             return View(thisClient);
         }
-        }}
+
+        public ActionResult Edit(int id)
+        {
+            var thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+            ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+            return View(thisClient);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Client client)
+        {
+            _db.Entry(client).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }}
