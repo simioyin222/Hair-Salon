045fd4f339d1a9af4cceb278fe0d01b2c14c3954
simioyin222 <simi.oyinkolade@gmail.com>
2024-02-03 02:34:41 -0600

    add edit and details functionality

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -33,4 +33,27 @@ namespace HairSalon.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult Details(int id)
+        {
+            var thisStylist = _db.Stylists
+                .Include(stylist => stylist.Clients)
+                .FirstOrDefault(stylist => stylist.StylistId == id);
+
+            return View(thisStylist);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var thisStylist = _db.Stylists.FirstOrDefault(stylists => stylists.StylistId == id);
+            return View(thisStylist);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Stylist stylist)
+        {
+            _db.Entry(stylist).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }}
